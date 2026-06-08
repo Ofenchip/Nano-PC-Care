a61aba3c95affc861ad704a396511f1da86df837
Oleksandr <r05414250@gmail.com>
2026-06-08 21:26:23 +0200

    script addition

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.getElementById("hero-button").onclick = () => {
-    alert("Working!");
-};
 
+const fixButton = document.querySelector(".hero-button");
+const message = document.querySelector("#message");
+
+fixButton.addEventListener("click", () => {
+    console.log("Let's begin PC fixing!");
+    message.textContent = "Let's begin PC fixing!";
+});
+
+const darkMode = document.querySelector(".dark-mode")
