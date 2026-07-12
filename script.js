5e93398f868573bbe1ae872303dbec7de3517760
Oleksandr <r05414250@gmail.com>
2026-07-12 22:03:36 +0200

    dark-mode addition

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
+const darkButton = document.querySelector(".dark-button");
+
+darkButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        darkButton.textContent = "☀️ Light Mode";
+    } else {
+        darkButton.textContent = "🌙 Dark Mode";
+    }
+});
+
 const fixButton = document.querySelector(".hero-button");
 const message = document.querySelector("#message");
 
@@ -6,5 +18,3 @@ fixButton.addEventListener("click", () => {
     console.log("Let's begin PC fixing!");
     message.textContent = "Let's begin PC fixing!";
 });
-
-const darkMode = document.querySelector(".dark-mode")
